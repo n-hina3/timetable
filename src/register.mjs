59ab1c8a2f782a5d3e913ce5c7150153fef51320
NakaHina <nakahina39@gmail.com>
2025-03-18 17:15:53 +0900

    floating-linkを追加

--- src/register.mjs
+++ src/register.mjs
@@ -1,4 +1,5 @@
 import { ClassListItemComponent } from "./components/class-list-item.mjs";
+import { FloatingLink } from "./components/floating-link.mjs";
 import { TimetableDetailComponent } from "./components/timetable-detail.mjs";
 import { TimetableComponent } from "./components/timetable.mjs";
 import { ClassEditPage } from "./pages/class-edit.mjs";
@@ -11,3 +12,4 @@ customElements.define("class-edit-page", ClassEditPage);
 customElements.define("class-list-item", ClassListItemComponent);
 customElements.define("timetable-component", TimetableComponent);
 customElements.define("timetable-detail", TimetableDetailComponent);
+customElements.define("floating-link", FloatingLink);
