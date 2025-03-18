e6cfdfcedf293ba2b79712c212ba91f60e8a9904
NakaHina <nakahina39@gmail.com>
2025-03-18 14:16:36 +0900

    8.3.表のコンポーネントをタイムテーブルコンポーネントを追加した

--- src/pages/home.mjs
+++ src/pages/home.mjs
@@ -1,10 +1,29 @@
+import { basicStyle } from "../shared/style.mjs";
 export class HomePage extends HTMLElement {
   /** @type {ShadowRoot | undefined} */
   shadowRoot = undefined;
 
-  html = () => /* html */ `
-    <span>home page</span>
+  css = () => /* css */ `
+    ${basicStyle}
+
+    :host .home {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+
+      & > timetable-component {
+        height: 60%;
+      }
+    }
   `;
+
+  html = () => /* html */ `
+  <style>${this.css()}</style>
+  <div class="home">
+    <timetable-component></timetable-component>
+  </div>
+`;
 
   constructor() {
     super();
