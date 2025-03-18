808a611c140816ba20b2bc4f3dde7c392b41b99d
NakaHina <nakahina39@gmail.com>
2025-03-18 15:55:01 +0900

    kiwi-maru-regular&style.mjs追加

---
+++ src/shared/style.mjs
@@ -0,0 +1,13 @@
+export const basicStyle = /*css*/ `
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.kiwi-maru-regular {
+  font-family: "Kiwi Maru", serif;
+  font-weight: 400;
+  font-style: normal;
+}
+
+`;
